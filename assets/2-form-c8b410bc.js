e15afccb5c74651eb9dfcb439f5062655def99b3
DmytroK-goit <159040488+DmytroK-goit@users.noreply.github.com>
2024-06-25 07:02:19 +0000

    Deploying to gh-pages from @ DmytroK-goit/goit-js-hw-09@d2f1a372ddc4c22e48c60bfedf5deb08d9d7241f 🚀

--- assets/2-form-c8b410bc.js
+++
@@ -1,2 +0,0 @@
-const a=document.querySelector(".feedback-form");let e={email:"",message:""};document.addEventListener("DOMContentLoaded",()=>{const t=localStorage.getItem("formData");t&&(e=JSON.parse(t),a.elements.email.value=e.email,a.elements.message.value=e.message)});a.addEventListener("input",t=>{const{name:o,value:m}=t.target;o in e&&(e[o]=m,localStorage.setItem("formData",JSON.stringify(e)))});a.addEventListener("submit",t=>{t.preventDefault(),console.log("Form submitted:",e),localStorage.removeItem("formData"),a.reset()});
-//# sourceMappingURL=2-form-c8b410bc.js.map
